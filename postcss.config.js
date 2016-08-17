23787c1cc6c0756baa43f7a3b6f9dcfa7e080ede
Michael Shick <m@shick.us>
2016-08-16 19:36:18 -0700

    update to latest generator

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'use': [
+    'postcss-custom-media', 'postcss-media-minmax', 'postcss-lh', 'postcss-pr',
+    'autoprefixer'
+  ],
+  'autoprefixer': {
+    'browsers': '> 5%'
+  }
+};
