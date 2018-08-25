ca267768e6fabf729f71ad46615d738556602251
Michael Shick <m@shick.us>
2018-08-25 16:11:38 -0400

    fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,24 @@
-'use strict';
-
 const config = {
-  use: [
-    'postcss-custom-media', 'postcss-media-minmax', 'postcss-lh',
-    'autoprefixer'
-  ],
-  autoprefixer: {
-    browsers: '> 5%'
-  },
-  'postcss-urlrev': {
-    relativePath: 'assets/stylesheets',
-    absolutePath: __dirname
+  plugins: {
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {},
+    'postcss-lh': {},
+    autoprefixer: {
+      browsers: '> 5%'
+    },
+    'postcss-urlrev': {
+      relativePath: 'assets/stylesheets',
+      absolutePath: __dirname
+    }
   }
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.use.push('postcss-urlrev');
-  config.use.push('cssnano');
+  config.plugins = {
+    ...config.plugins,
+    'postcss-urlrev': {},
+    cssnano: {}
+  };
 }
 
 module.exports = config;
